5dd910c596241c0073bd1b8808dcd861f11131c5
Sajuly <107915361+Sajuly@users.noreply.github.com>
2022-09-23 11:17:41 +0300

    Update main.sh

--- 05/main.sh
+++ 05/main.sh
@@ -2,13 +2,15 @@
 
 if [ $# != 1 ]; then
         echo "n/a"
-elif [[ "${1: -1}" != "/" ]]; then
-        echo "n/a1"
-elif [[ -d "$1" ]]; then
-        end=$(echo $1 | tail -c 2)
+# elif [[ "${1: -1}" != "/" ]]; then  # проверка последнего символа
+#         echo "n/a1"
+elif [[ -d "$1" ]]; then # -d проверка file is a directory
+        end=$(echo $1 | tail -c 2)  # символ в конце строки
         if [[ "$end" = "/" ]]; then
                 export param=$1
                 # sudo chmod +x info.sh
                 ./info.sh
         fi
+else 
+        echo "n/a"
 fi
